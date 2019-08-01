ead1cdf334949d0f35b1f42aabccb7a38f863ac6
Eduard Barbulescu <eduard.barbulescu@gmail.com>
2019-08-02 03:51:19 +0530

    Update task_config.py

--- task_config.py
+++ task_config.py
@@ -13,8 +13,8 @@ SuperGLUE_TASK_SPLIT_MAPPING = {
 SuperGLUE_LABEL_MAPPING = {
     "CB": {"entailment": 1, "contradiction": 2, "neutral": 3},
     "COPA": {0: 1, 1: 2},
-    "RTE": {"entailment": 1, "not_entailment": 2},
-    #"RTE": {True: 1, False: 2},
+    #"RTE": {"entailment": 1, "not_entailment": 2},
+    "RTE": {True: 1, False: 2},
     "WiC": {True: 1, False: 2},
     "WSC": {True: 1, False: 2},
     "MultiRC": {True: 1, False: 2},
